8b0cb8a6384f2b0a870241160bd48bc8d88ed19e
ashok <classicashok63@gmail.com>
2019-12-24 08:14:26 +0545

    final heroku commit

--- tasbir/settings.py
+++ tasbir/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,3 +133,5 @@ MEDIA_URL = '/media/'
 if 'DATABASE_URL' in os.environ: #means heroku 
     import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
